ac60d6a499aad353af1efc496c2761de630d703a
Dan <daniel.weaver@gmail.com>
2009-04-21 21:13:35 -0600

    more

--- twitter.rb
+++ twitter.rb
@@ -3,9 +3,7 @@
 require 'rubygems'
 require 'hpricot'
 require 'net/http'
- 
-$username = 'snf_sandbox'
-$password = 'platte16'
+require 'config.rb'
  
 def twitter(command, type=:get, opts={})
     twitter = Net::HTTP.start('twitter.com')
@@ -29,20 +27,28 @@ def twitter(command, type=:get, opts={})
 end
  
 # doc = twitter('/statuses/friends_timeline.xml')
-# doc = twitter('/users/show/aboutruby.xml')
+# doc = twitter('/statuses/show.xml', :get, {:id => 1374254546})
 # doc = twitter('/statuses/user_timeline.xml', :get, {:id => 'snf', :count => 5})
 # doc = twitter('/statuses/destroy/15788551193557601.xml', :post)
-doc = twitter('/statuses/update.xml', :post, {:status => "First tweet"})
+doc = twitter('/statuses/update.xml', :post, {:status => "Globals"})
+ 
  
 (doc/'status').each do |st|
-    user = (st/'user name').inner_html
-    text = (st/'text').inner_html
-    followers = (st/'followers_count').inner_html
-    following = (st/'friends_count').inner_html
-    following_back = (st/'follow').inner_html
-    created_at = (st/'created_at').inner_html
-    status_id = (st/'id').inner_html
-    puts "#{status_id}: #{text}"
+  text = (st/'text').inner_html
+  followers = (st/'followers_count').inner_html
+  following = (st/'friends_count').inner_html
+  following_back = (st/'follow').inner_html
+  created_at = (st/'created_at').inner_html
+  status_id = (st/'id').inner_html
+  (st/'user').each do |u|
+    @user = (st/'user_name').inner_html
+    @user_id = (st/'id').inner_html
+    @user_website = (st/'url').inner_html
+  end
+  puts "#{@user}-(ID:#{@user_id})"
+  puts "Website: #{@user_website}"
+  puts "Date: #{created_at}"
+  puts "#{text}"
 end
 
 (doc/'hash').each do |response|
